1ece40f1896e6e0b8aa8cce5c63da8bc123570b1
SadiaShakiba <sakiba26999@gmail.com>
2025-01-19 14:24:54 +0600

    frontend: home page added

---
+++ src/components/Home/TopMeal.js
@@ -0,0 +1,47 @@
+export const topMeals = [
+  {
+    image:
+      "https://images.pexels.com/photos/3590401/pexels-photo-3590401.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Pasta",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1639557/pexels-photo-1639557.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Burger",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/2955819/pexels-photo-2955819.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Shwarma",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Salad",
+  },
+  {
+    image:
+      "https://images.pexels.com/photos/539451/pexels-photo-539451.jpeg?auto=compress&cs=tinysrgb&w=600",
+    title: "Soup",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/05/10/17/27/sandwich-2301387_640.jpg",
+    title: "Sandwich",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2022/06/07/20/52/curry-7249247_1280.jpg",
+    title: "Chicken",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2022/11/18/05/40/biriyani-7599454_640.jpg",
+    title: "Biriyani",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/07/16/12/39/chicken-2509164_640.jpg",
+    title: "Steak",
+  },
+];
