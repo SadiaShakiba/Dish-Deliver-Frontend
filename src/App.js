4a3b03b6e4121497360d5498bcc074859cc1cf53
SadiaShakiba <sakiba26999@gmail.com>
2025-01-21 10:55:52 +0600

    frontend: full page added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./App.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { lightTheme } from "./Theme/LightTheme";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import RestaurantDetails from "./components/Restaurant/RestaurantDetails";
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <CssBaseline />
       <Navbar />
+      {/* <Home /> */}
+      <RestaurantDetails />
+      <Footer />
     </ThemeProvider>
   );
 }
